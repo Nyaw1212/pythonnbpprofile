3dd859717a03621cd176cf9aeb733aa355b45150
Nyaw1212 <alvin.chiao@mayersolar.com>
2026-08-18 15:34:39 +0800

    Test multiline search ordering

--- tests/test_personnel_service.py
+++ tests/test_personnel_service.py
@@ -16,12 +16,14 @@ class PersonnelServiceTests(unittest.TestCase):
                 """
                 INSERT INTO personnel (
                     badge_number, rank, last_name, first_name, middle_name,
-                    camp, office, classification, personnel_type
-                ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+                    camp, office, classification, personnel_type, source_order
+                ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
                 """,
                 [
-                    ("2", "CSSUPT", "GARCIA", "GARY", "A", "NBP", "OTS", "Commissioned", "CORRECTIONS OFFICER"),
-                    ("7", "CCINSP", "BUTAWAN", "ROBERTO", None, "NBP", "ESCORT", None, "CORRECTIONS OFFICER"),
+                    ("2", "CSSUPT", "GARCIA", "GARY", "A", "NBP", "OTS", "Commissioned", "CORRECTIONS OFFICER", 2),
+                    ("7", "CCINSP", "BUTAWAN", "ROBERTO", None, "NBP", "ESCORT", None, "CORRECTIONS OFFICER", 1),
+                    ("10", "CO1", "CHIAO", "ALVIN", None, "NBP", "CASO", None, "CORRECTIONS OFFICER", 4),
+                    ("11", "CO1", "CANAO", "JUAN", None, "NBP", "CASO", None, "CORRECTIONS OFFICER", 3),
                 ],
             )
         self.service = PersonnelService(self.db_path)
@@ -43,6 +45,17 @@ class PersonnelServiceTests(unittest.TestCase):
         profile = self.service.get_profile("2")
         self.assertIsNotNone(profile)
         self.assertEqual(profile["rank"], "CSSUPT")
+
+    def test_multiline_search_preserves_term_order(self):
+        result = self.service.search("CHIAO\nCANAO")
+        self.assertEqual([row["last_name"] for row in result], ["CHIAO", "CANAO"])
+
+    def test_default_search_preserves_sheet_order(self):
+        result = self.service.search(limit=4)
+        self.assertEqual(
+            [row["badge_number"] for row in result],
+            ["7", "2", "11", "10"],
+        )
 
 
 if __name__ == "__main__":
